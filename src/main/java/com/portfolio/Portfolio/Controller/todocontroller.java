4f621110a069334d1437b31b561351dbad033890
RenzoMag <renzomagnoni203@gmail.com>
2023-03-25 15:24:57 -0300

    config dockerfile

--- src/main/java/com/portfolio/Portfolio/Controller/todocontroller.java
+++ src/main/java/com/portfolio/Portfolio/Controller/todocontroller.java
@@ -3,7 +3,6 @@ package com.portfolio.Portfolio.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import com.portfolio.Portfolio.model.Task;
 import com.portfolio.Portfolio.model.exp;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200/")
 public class todocontroller {
     @Autowired
     private TodoRepository todoRepository;
